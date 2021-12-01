a464af96733797767186d03107451e0c6d72c33c
Marc Kelsey <marc.kelsey@textnow.com>
2021-12-01 13:33:13 -0500

    01a

--- 01/solution.rb
+++ 01/solution.rb
@@ -1,9 +1,7 @@
 require 'byebug'
 
-require 'byebug'
-
 def transform(input)
-  input.strip.split("\n")
+  input.strip.split("\n").map(&:to_i)
 end
 
 def input
@@ -23,6 +21,16 @@ def test_input
   return @test_input if @test_input
 
   raw_test_input = <<~TESTINPUT
+    199
+    200
+    208
+    210
+    200
+    207
+    240
+    269
+    260
+    263
   TESTINPUT
 
   @test_input = transform(raw_test_input)
@@ -34,16 +42,24 @@ def assert(value, expected_value)
 end
 
 # stuff for this problem
-def solution1(_input)
-  'Not implemented!'
+def solution1(input)
+  previous_depth = input.shift
+  greater = 0
+
+  input.each do |current_depth|
+    greater += 1 if current_depth > previous_depth
+    previous_depth = current_depth
+  end
+
+  greater
 end
 
 def solution2(_input)
   'Not implemented!'
 end
 
-assert(solution1(@test_input), nil)
-assert(solution2(@test_input), nil)
+assert(solution1(test_input), 7)
+# assert(solution2(@test_input), nil)
 
-puts "Solution 1: #{solution1(@input)}"
-puts "Solution 2: #{solution2(@input)}"
+puts "Solution 1: #{solution1(input)}"
+# puts "Solution 2: #{solution2(@input)}"
